1c7e2e89fde5a13d00a750b4fdd2a0b65b136360
E.Azer Koçulu <azer@kodfabrik.com>
2013-11-25 00:07:51 -0800

    better tests

--- test.js
+++ test.js
@@ -3,8 +3,18 @@ var midi = require("./")('http://localhost:3000', ['acoustic_grand_piano']);
 it('requires midi.js sdk', function(done){
   midi(function () {
     MIDI.noteOn(1, 55, 123, 0);
-    MIDI.noteOn(1, 55, 123, 0.25);
-    MIDI.noteOn(1, 55, 123, 0.75);
-    done();
   });
+
+  midi(function () {
+    MIDI.noteOn(1, 75, 123, 0.4);
+  });
+
+  midi(function () {
+    MIDI.noteOn(1, 100, 123, 0.8);
+  });
+
+  setTimeout(function () {
+    MIDI.noteOn(1, 40, 123, 1.2);
+    done();
+  }, 600);
 });
